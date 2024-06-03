0fcf16cef138dd485de3817b480023cb468dd25b
ArtemHvozdov <naltuhov4@gmail.com>
2024-06-03 19:57:04 +0300

    add first file

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	exampleText := "Hello, new part of my life!"
+
+	exampleText2 := "Hello, second variable!"
+
+	fmt.Println(exampleText)
+
+	fmt.Println(exampleText2)
+}
